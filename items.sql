e1ab7babef581f170b147cd71a57dc82fd49c3b3
weslley <weslleyvwp@hotmail.com>
2024-04-11 14:45:18 -0300

    test: addition

---
+++ items.sql
@@ -0,0 +1,408 @@
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 1),
+       ('Mais ou menos', 1, 1, 1),
+       ('Verdade', 0, 2, 1);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 2),
+       ('Mais ou menos', 1, 1, 2),
+       ('Verdade', 0, 2, 2);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 3),
+       ('Mais ou menos', 1, 1, 3),
+       ('Verdade', 0, 2, 3);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 4),
+       ('Mais ou menos', 1, 1, 4),
+       ('Verdade', 0, 2, 4);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 5),
+       ('Mais ou menos', 1, 1, 5),
+       ('Verdade', 0, 2, 5);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 6),
+       ('Mais ou menos', 1, 1, 6),
+       ('Verdade', 0, 2, 6);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 7),
+       ('Mais ou menos', 1, 1, 7),
+       ('Verdade', 0, 2, 7);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 8),
+       ('Mais ou menos', 1, 1, 8),
+       ('Verdade', 0, 2, 8);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 9),
+       ('Mais ou menos', 1, 1, 9),
+       ('Verdade', 0, 2, 9);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 10),
+       ('Mais ou menos', 1, 1, 10),
+       ('Verdade', 0, 2, 10);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 11),
+       ('Mais ou menos', 1, 1, 11),
+       ('Verdade', 0, 2, 11);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 12),
+       ('Mais ou menos', 1, 1, 12),
+       ('Verdade', 0, 2, 12);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 13),
+       ('Mais ou menos', 1, 1, 13),
+       ('Verdade', 0, 2, 13);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não verdadeiro', 2, 0, 14),
+       ('Mais ou menos', 1, 1, 14),
+       ('Verdade', 0, 2, 14);
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 15),
+       ('Mais ou menos', 1, 1, 15),
+       ('Descreve o meu filho', 0, 2, 15);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 16),
+       ('Mais ou menos', 1, 1, 16),
+       ('Descreve o meu filho', 0, 2, 16);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 17),
+       ('Mais ou menos', 1, 1, 17),
+       ('Descreve o meu filho', 0, 2, 17);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 18),
+       ('Mais ou menos', 1, 1, 18),
+       ('Descreve o meu filho', 0, 2, 18);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 19),
+       ('Mais ou menos', 1, 1, 19),
+       ('Descreve o meu filho', 0, 2, 19);
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 20),
+       ('Mais ou menos', 1, 1, 20),
+       ('Descreve o meu filho', 0, 2, 20);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 21),
+       ('Mais ou menos', 1, 1, 21),
+       ('Descreve o meu filho', 0, 2, 21);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 22),
+       ('Mais ou menos', 1, 1, 22),
+       ('Descreve o meu filho', 0, 2, 22);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 23),
+       ('Mais ou menos', 1, 1, 23),
+       ('Descreve o meu filho', 0, 2, 23);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 24),
+       ('Mais ou menos', 1, 1, 24),
+       ('Descreve o meu filho', 0, 2, 24);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 25),
+       ('Mais ou menos', 1, 1, 25),
+       ('Descreve o meu filho', 0, 2, 25);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 26),
+       ('Mais ou menos', 1, 1, 26),
+       ('Descreve o meu filho', 0, 2, 26);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 27),
+       ('Mais ou menos', 1, 1, 27),
+       ('Descreve o meu filho', 0, 2, 27);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 28),
+       ('Mais ou menos', 1, 1, 28),
+       ('Descreve o meu filho', 0, 2, 28);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 29),
+       ('Mais ou menos', 1, 1, 29),
+       ('Descreve o meu filho', 0, 2, 29);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 30),
+       ('Mais ou menos', 1, 1, 30),
+       ('Descreve o meu filho', 0, 2, 30);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 31),
+       ('Mais ou menos', 1, 1, 31),
+       ('Descreve o meu filho', 0, 2, 31);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 32),
+       ('Mais ou menos', 1, 1, 32),
+       ('Descreve o meu filho', 0, 2, 32);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 33),
+       ('Mais ou menos', 1, 1, 33),
+       ('Descreve o meu filho', 0, 2, 33);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descritivo', 2, 0, 34),
+       ('Mais ou menos', 1, 1, 34),
+       ('Descreve o meu filho', 0, 2, 34);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 35),
+       ('Descreve um pouco', 1, 1, 35),
+       ('Descreve o meu filho', 0, 2, 35);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 36),
+       ('Descreve um pouco', 1, 1, 36),
+       ('Descreve o meu filho', 0, 2, 36);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 37),
+       ('Descreve um pouco', 1, 1, 37),
+       ('Descreve o meu filho', 0, 2, 37);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 38),
+       ('Descreve um pouco', 1, 1, 38),
+       ('Descreve o meu filho', 0, 2, 38);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 39),
+       ('Descreve um pouco', 1, 1, 39),
+       ('Descreve o meu filho', 0, 2, 39);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 40),
+       ('Descreve um pouco', 1, 1, 40),
+       ('Descreve o meu filho', 0, 2, 40);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 41),
+       ('Descreve um pouco', 1, 1, 41),
+       ('Descreve o meu filho', 0, 2, 41);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 42),
+       ('Descreve um pouco', 1, 1, 42),
+       ('Descreve o meu filho', 0, 2, 42);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 43),
+       ('Descreve um pouco', 1, 1, 43),
+       ('Descreve o meu filho', 0, 2, 43);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 44),
+       ('Descreve um pouco', 1, 1, 44),
+       ('Descreve o meu filho', 0, 2, 44);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 45),
+       ('Descreve um pouco', 1, 1, 45),
+       ('Descreve o meu filho', 0, 2, 45);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 46),
+       ('Descreve um pouco', 1, 1, 46),
+       ('Descreve o meu filho', 0, 2, 46);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 47),
+       ('Descreve um pouco', 1, 1, 47),
+       ('Descreve o meu filho', 0, 2, 47);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 48),
+       ('Descreve um pouco', 1, 1, 48),
+       ('Descreve o meu filho', 0, 2, 48);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 49),
+       ('Descreve um pouco', 1, 1, 49),
+       ('Descreve o meu filho', 0, 2, 49);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 50),
+       ('Descreve um pouco', 1, 1, 50),
+       ('Descreve o meu filho', 0, 2, 50);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 51),
+       ('Descreve um pouco', 1, 1, 51),
+       ('Descreve o meu filho', 0, 2, 51);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não descreve meu filho', 2, 0, 52),
+       ('Descreve um pouco', 1, 1, 52),
+       ('Descreve o meu filho', 0, 2, 52);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 53),
+       ('Problema menor', 1, 1, 53),
+       ('Problema moderado', 0, 2, 53),
+       ('Problema sério', 0, 3, 53);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 54),
+       ('Problema menor', 1, 1, 54),
+       ('Problema moderado', 0, 2, 54),
+       ('Problema sério', 0, 3, 54);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 55),
+       ('Problema menor', 1, 1, 55),
+       ('Problema moderado', 0, 2, 55),
+       ('Problema sério', 0, 3, 55);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 56),
+       ('Problema menor', 1, 1, 56),
+       ('Problema moderado', 0, 2, 56),
+       ('Problema sério', 0, 3, 56);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 57),
+       ('Problema menor', 1, 1, 57),
+       ('Problema moderado', 0, 2, 57),
+       ('Problema sério', 0, 3, 57);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 58),
+       ('Problema menor', 1, 1, 58),
+       ('Problema moderado', 0, 2, 58),
+       ('Problema sério', 0, 3, 58);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 59),
+       ('Problema menor', 1, 1, 59),
+       ('Problema moderado', 0, 2, 59),
+       ('Problema sério', 0, 3, 59);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 60),
+       ('Problema menor', 1, 1, 60),
+       ('Problema moderado', 0, 2, 60),
+       ('Problema sério', 0, 3, 60);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 61),
+       ('Problema menor', 1, 1, 61),
+       ('Problema moderado', 0, 2, 61),
+       ('Problema sério', 0, 3, 61);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 62),
+       ('Problema menor', 1, 1, 62),
+       ('Problema moderado', 0, 2, 62),
+       ('Problema sério', 0, 3, 62);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 63),
+       ('Problema menor', 1, 1, 63),
+       ('Problema moderado', 0, 2, 63),
+       ('Problema sério', 0, 3, 63);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 64),
+       ('Problema menor', 1, 1, 64),
+       ('Problema moderado', 0, 2, 64),
+       ('Problema sério', 0, 3, 64);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 65),
+       ('Problema menor', 1, 1, 65),
+       ('Problema moderado', 0, 2, 65),
+       ('Problema sério', 0, 3, 65);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 66),
+       ('Problema menor', 1, 1, 66),
+       ('Problema moderado', 0, 2, 66),
+       ('Problema sério', 0, 3, 66);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 67),
+       ('Problema menor', 1, 1, 67),
+       ('Problema moderado', 0, 2, 67),
+       ('Problema sério', 0, 3, 67);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 68),
+       ('Problema menor', 1, 1, 68),
+       ('Problema moderado', 0, 2, 68),
+       ('Problema sério', 0, 3, 68);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 69),
+       ('Problema menor', 1, 1, 69),
+       ('Problema moderado', 0, 2, 69),
+       ('Problema sério', 0, 3, 69);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 70),
+       ('Problema menor', 1, 1, 70),
+       ('Problema moderado', 0, 2, 70),
+       ('Problema sério', 0, 3, 70);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 71),
+       ('Problema menor', 1, 1, 71),
+       ('Problema moderado', 0, 2, 71),
+       ('Problema sério', 0, 3, 71);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 72),
+       ('Problema menor', 1, 1, 72),
+       ('Problema moderado', 0, 2, 72),
+       ('Problema sério', 0, 3, 72);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 73),
+       ('Problema menor', 1, 1, 73),
+       ('Problema moderado', 0, 2, 73),
+       ('Problema sério', 0, 3, 73);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 74),
+       ('Problema menor', 1, 1, 74),
+       ('Problema moderado', 0, 2, 74),
+       ('Problema sério', 0, 3, 74);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 75),
+       ('Problema menor', 1, 1, 75),
+       ('Problema moderado', 0, 2, 75),
+       ('Problema sério', 0, 3, 75);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 76),
+       ('Problema menor', 1, 1, 76),
+       ('Problema moderado', 0, 2, 76),
+       ('Problema sério', 0, 3, 76);
+
+INSERT INTO item (content, number, score, question)
+VALUES ('Não é um problema', 2, 0, 77),
+       ('Problema menor', 1, 1, 77),
+       ('Problema moderado', 0, 2, 77),
+       ('Problema sério', 0, 3, 77);
+
